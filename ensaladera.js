c7f40a44e4ffc1c095153ad4de5aad4ed75d27d0
comision20formar <133821977+comision20formar@users.noreply.github.com>
2023-05-22 20:15:22 -0300

    Update ensaladera.js

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log('Aquí está la ensaladera')
+console.log('Instrucciones')
+console.log(`
+1. Lavar las verduras
+2. Cortar a mano la lechuga
+3. Cortar el toma en rodajas, a 45° grados
+4. Cortar la cebolla a la juliana
+`)
